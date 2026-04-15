d4b25b9b486b7c1da6ec064a52449663ac9cb153
IdrisSyaifulloh <saefullahiddris@gmail.com>
2026-04-16 01:51:57 +0700

    Update

--- db/create_operator.sql
+++ db/create_operator.sql
@@ -1,7 +1,10 @@
 BEGIN;
 
 INSERT INTO users (id, name, initials, role, email, password_hash, prodi)
-VALUES ('OP001', 'Admin Operator', 'AO', 'operator', 'operator@ac.id', md5('Operator#2026'), NULL)
+VALUES
+  ('OP001', 'idrssyfllh', 'IDR', 'operator', 'idrssyfllh@ac.id', '$2b$10$vm8m2hd/mqJkdfJp8Al6V.XSv9iThHd1liCfiLq.IPJN2CDNpzYJW', NULL),
+  ('OP002', 'irham', 'IRH', 'operator', 'irham@ac.id', '$2b$10$vm8m2hd/mqJkdfJp8Al6V.XSv9iThHd1liCfiLq.IPJN2CDNpzYJW', NULL),
+  ('OP003', 'rey', 'REY', 'operator', 'rey@ac.id', '$2b$10$vm8m2hd/mqJkdfJp8Al6V.XSv9iThHd1liCfiLq.IPJN2CDNpzYJW', NULL)
 ON CONFLICT (id)
 DO UPDATE SET
   name = EXCLUDED.name,
