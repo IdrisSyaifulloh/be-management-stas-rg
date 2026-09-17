167bdcbfae911b7686c4edba320f71d225b64831
IdrisSyaifulloh <saefullahiddris@gmail.com>
2026-09-17 20:04:47 +0700

    Merge branch 'origin/main'

--- db/seed.sql
+++ db/seed.sql
@@ -319,7 +319,7 @@ ON CONFLICT (id) DO UPDATE SET
   estimasi = EXCLUDED.estimasi,
   nomor_surat = EXCLUDED.nomor_surat,
   file_url = EXCLUDED.file_url,
-  updated_at = NOW();
+  updated_at = NOW();a
 
 -- ======================================================
 -- CERTIFICATE REQUESTS
